8d9153b0b737ecd3e3dbb3b0b11c6c266eef18fe
Saurabh8824 <jaimahakal6377@gmail.com>
2026-05-17 20:59:16 +0530

    Add files via upload

--- login/views.py
+++ login/views.py
@@ -3,41 +3,76 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import logout
+from qr_app.models import UserProfile
 
 def user_login(request):
+	
+	    # ✅ already logged in
+    if request.user.is_authenticated:
+
+        try:
+
+            if request.user.userprofile.role == "teacher":
+                return redirect("home")
+
+            else:
+                return redirect("student_dashboard")
+
+        except:
+            pass
+
     if request.method == "POST":
 
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
 
         if user is not None:
+
+            # 🔥 clear old session
+            logout(request)
+
+            # 🔥 login new user
             login(request, user)
 
-            # Admin
+            # 🔥 admin redirect
             if user.is_superuser:
                 return redirect("/admin/")
 
-            # 🔥 SAFE ROLE FETCH
+            # 🔥 safe role fetch
             try:
                 role = user.userprofile.role
-            except:
+
+            except UserProfile.DoesNotExist:
                 role = "student"
 
+            # 🔥 role redirect
             if role == "teacher":
                 return redirect("home")
+
             else:
                 return redirect("student_dashboard")
 
         else:
-            messages.error(request, "Invalid Username or Password")
+
+            messages.error(
+                request,
+                "Invalid Username or Password"
+            )
 
     return render(request, "login/login.html")
 
 
 
-
 def user_logout(request):
+
+    request.session.flush()
+
     logout(request)
+
     return redirect("user_login")
